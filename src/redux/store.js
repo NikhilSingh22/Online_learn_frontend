5c24f8aab89348e47473fbee8710376fb415c857
Nikhil Singh <ns2125560@gmail.com>
2023-07-10 18:52:44 +0530

    server link changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,4 +20,4 @@ const store = configureStore({
 
 export default store;
 
-export const server = 'https://coursebundler.herokuapp.com/api/v1';
+export const server = 'https://online-learn-backend.onrender.com/api/v1';
